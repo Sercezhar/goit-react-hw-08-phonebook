ba725640e5afc94be66bf2b1cabda39f81c8c0e9
Sercezhar <87950995+Sercezhar@users.noreply.github.com>
2022-06-26 08:37:08 +0000

    Deploying to gh-pages from @ Sercezhar/goit-react-hw-08-phonebook@b88f97963b90556f09f59cd1a7af3d908780bf47 🚀

---
+++ static/js/469.c122a706.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[469],{1469:function(e,n,o){o.r(n),o.d(n,{HomePage:function(){return _},default:function(){return c}});var t="HomePage_Container__1wZVs",a="HomePage_Title__x++dF",r=o(3329);function _(){return(0,r.jsx)("div",{className:t,children:(0,r.jsx)("h1",{className:a,children:"YOUR CONTACTS IN ONE PLACE"})})}var c=_}}]);
+//# sourceMappingURL=469.c122a706.chunk.js.map
